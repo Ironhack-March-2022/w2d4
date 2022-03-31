53995784f3cdbba02433723687485481f6911949
Jan Redmann <janredmann1@gmail.com>
2022-03-31 11:23:24 +0200

    add all tests

--- testing-intro/spec/greeting.spec.js
+++ testing-intro/spec/greeting.spec.js
@@ -1,1 +1,14 @@
-//
+// this imports the greeting function
+
+const greeting = require('../src/greeting')
+// terminology: test suite
+describe('The function greeting', function () {
+	// spec - specification
+	it('should be a function', function () {
+		// expectation - using a matcher
+		expect(typeof greeting).toBe('function')
+	})
+	it('should return a greeting', function () {
+		expect(greeting()).toEqual('hello ironhackers!');
+	});
+})
